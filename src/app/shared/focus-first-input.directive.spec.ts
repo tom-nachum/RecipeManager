e0b1e32ccccc8e907ddbee6c4315274fe9d216de
tom-nachum <tom.nachum@gmail.com>
2022-04-30 14:07:03 +0300

    fixed tooltip width

---
+++ src/app/shared/focus-first-input.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FocusFirstInputDirective } from './focus-first-input.directive';
+
+describe('FocusFirstInputDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FocusFirstInputDirective();
+    expect(directive).toBeTruthy();
+  });
+});
